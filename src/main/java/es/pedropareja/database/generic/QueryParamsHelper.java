f575360538262b94eff932d9fb880dc5cbd46610
Pedro Francisco Pareja Ruiz <pedropareja@gmail.com>
2019-08-14 14:13:31 +0200

    QueryParamsHelper: param setters generalization

--- src/main/java/es/pedropareja/database/generic/QueryParamsHelper.java
+++ src/main/java/es/pedropareja/database/generic/QueryParamsHelper.java
@@ -6,6 +6,9 @@ import java.util.Collection;
 
 public class QueryParamsHelper
 {
+    private static final ParamSetter<Long> longSetter =  (s,i,v)-> s.setLong(i, v);
+    private static final ParamSetter<String> stringSetter =  (s,i,v)-> s.setString(i, v);
+
     private final PreparedStatement statement;
     private int paramIndex = 1;
 
@@ -20,39 +23,64 @@ public class QueryParamsHelper
             paramSetter.setParam(statement, paramIndex++, value);
     }
 
+    private <T> void setParam(ParamSetter<T> paramSetter, T value, boolean condition) throws SQLException
+    {
+        if(condition)
+            setParam(paramSetter, value);
+    }
+
+    private <T> void setAllParam(ParamSetter<T> paramSetter, Collection<T> collection) throws SQLException
+    {
+        if(collection != null)
+            for(T element: collection)
+                setParam(paramSetter, element);
+    }
+
+    private <T> void setAllParam(ParamSetter<T> paramSetter, Collection<T> collection, boolean condition) throws SQLException
+    {
+        if(condition)
+            setAllParam(paramSetter, collection);
+    }
+
+
     public void setLong(Long value) throws SQLException
     {
-        setParam((s,i,v)-> s.setLong(i, v), value);
+        setParam(longSetter, value);
     }
 
     public void setLong(Long value, boolean condition) throws SQLException
     {
-        if(condition)
-            setLong(value);
+        setParam(longSetter, value, condition);
     }
 
     public void setAllLong(Collection<Long> collection) throws SQLException
     {
-        if(collection != null)
-            for(Long value: collection)
-                setLong(value);
+        setAllParam(longSetter, collection);
     }
 
     public void setAllLong(Collection<Long> collection, boolean condition) throws SQLException
     {
-        if(condition)
-            setAllLong(collection);
+        setAllParam(longSetter, collection, condition);
     }
 
     public void setString(String value) throws SQLException
     {
-        setParam((s,i,v)-> s.setString(i, v), value);
+        setParam(stringSetter, value);
     }
 
     public void setString(String value, boolean condition) throws SQLException
     {
-        if(condition)
-            setString(value);
+        setParam(stringSetter, value, condition);
+    }
+
+    public void setAllString(Collection<String> collection) throws SQLException
+    {
+        setAllParam(stringSetter, collection);
+    }
+
+    public void setAllString(Collection<String> collection, boolean condition) throws SQLException
+    {
+        setAllParam(stringSetter, collection, condition);
     }
 
     public <U, F extends Enum<?> & DBFilterProcessor<U>> void applyFilter(Class<F> filterProcessorType, U filter)
