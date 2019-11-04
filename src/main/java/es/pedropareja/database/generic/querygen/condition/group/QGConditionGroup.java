f82c9bcf27b9b40ff0c00a7158200f8bb05218cf
Pedro Francisco Pareja Ruiz <pedropareja@gmail.com>
2019-11-04 09:58:55 +0100

    Generic condition group dev

--- src/main/java/es/pedropareja/database/generic/querygen/condition/group/QGConditionGroup.java
+++ src/main/java/es/pedropareja/database/generic/querygen/condition/group/QGConditionGroup.java
@@ -1,9 +1,11 @@
 package es.pedropareja.database.generic.querygen.condition.group;
 
+import es.pedropareja.database.generic.querygen.base.QGInitReferenced;
 import es.pedropareja.database.generic.querygen.base.QGOptionalityEnabled;
 import es.pedropareja.database.generic.querygen.condition.QGLinkConditions;
 
-public interface QGConditionGroup<T extends QGOptionalityEnabled> extends QGLinkConditions<QGConditionAll<T>>
+public interface QGConditionGroup<U extends QGConditionGroup<U,T>, T extends QGOptionalityEnabled & QGInitReferenced>
+        extends QGLinkConditions<U>, QGOptionalityEnabled, QGInitReferenced
 {
 
 }
--- src/main/java/es/pedropareja/database/generic/querygen/condition/group/QGConditionGroupPrv.java
+++ src/main/java/es/pedropareja/database/generic/querygen/condition/group/QGConditionGroupPrv.java
@@ -1,12 +1,44 @@
 package es.pedropareja.database.generic.querygen.condition.group;
 
-import es.pedropareja.database.generic.querygen.base.QGQuery;
-import es.pedropareja.database.generic.querygen.base.QGQueryMiddleEnd;
+import es.pedropareja.database.generic.querygen.base.QGInitReferenced;
+import es.pedropareja.database.generic.querygen.base.QGOptionalityEnabled;
+import es.pedropareja.database.generic.querygen.base.QGQueryInit;
+import es.pedropareja.database.generic.querygen.condition.QGConditionBase;
 import es.pedropareja.database.generic.querygen.condition.QGLinkConditionsPrv;
 
-public class QGConditionGroupPrv<T extends QGQuery, U extends QGConditionGroup<T>>
-    extends QGQueryMiddleEnd
-    implements QGConditionGroup<T>, QGLinkConditionsPrv<>
+import java.util.List;
+
+public class QGConditionGroupPrv<U extends QGConditionGroup<U,T>, T extends QGOptionalityEnabled & QGInitReferenced>
+    implements QGConditionGroup<U,T>, QGLinkConditionsPrv<U>
 {
 
+    @Override
+    public QGQueryInit getInit()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean getNextOptionalAppearanceValueAndReset()
+    {
+        return false;
+    }
+
+    @Override
+    public void setNextOptionalAppearanceValue(boolean nextOptionalAppearanceValue)
+    {
+
+    }
+
+    @Override
+    public List<QGConditionBase> getConditionList()
+    {
+        return null;
+    }
+
+    @Override
+    public U getThis()
+    {
+        return null;
+    }
 }
