ba08606027eb298e0743014d17e2a8a1ffaa8599
Pedro Francisco Pareja Ruiz <pedropareja@gmail.com>
2019-11-14 17:09:40 +0100

    Expression AS linked to Aggregations

--- src/main/java/es/pedropareja/database/generic/querygen/expression/aggregate/QGAggregate.java
+++ src/main/java/es/pedropareja/database/generic/querygen/expression/aggregate/QGAggregate.java
@@ -1,8 +1,9 @@
 package es.pedropareja.database.generic.querygen.expression.aggregate;
 
+import es.pedropareja.database.generic.querygen.expression.as.QGLinkAs;
 import es.pedropareja.database.generic.querygen.expression.base.QGExpression;
 
-public interface QGAggregate extends QGExpression
+public interface QGAggregate extends QGExpression, QGLinkAs
 {
     enum Type
     {
--- src/main/java/es/pedropareja/database/generic/querygen/expression/aggregate/QGAggregatePrv.java
+++ src/main/java/es/pedropareja/database/generic/querygen/expression/aggregate/QGAggregatePrv.java
@@ -2,6 +2,7 @@ package es.pedropareja.database.generic.querygen.expression.aggregate;
 
 import es.pedropareja.database.generic.DBFieldInfo;
 import es.pedropareja.database.generic.querygen.base.QGQueryBase;
+import es.pedropareja.database.generic.querygen.expression.as.QGLinkAsPrv;
 import es.pedropareja.database.generic.querygen.expression.base.QGExpressionBase;
 import es.pedropareja.database.generic.querygen.expression.base.QGExpressionPrv;
 
@@ -11,7 +12,7 @@ import java.util.Objects;
 import static es.pedropareja.database.generic.querygen.base.QGQueryBase.joinLists;
 
 public class QGAggregatePrv<T extends Enum<?> & DBFieldInfo>
-        extends QGExpressionBase implements QGAggregate
+        extends QGExpressionBase implements QGAggregate, QGLinkAsPrv
 {
     private final Type type;
     private final T field;
