7f23ebb8509bb5fdd9b0937ad6b5472b3abdd33d
pape9001 <PedroFrancisco.ParejaRuiz@nielsen.com>
2019-11-15 12:41:38 +0100

    Merge development

---
+++ src/main/java/es/pedropareja/database/generic/querygen/merge/QGMerge.java
@@ -0,0 +1,8 @@
+package es.pedropareja.database.generic.querygen.merge;
+
+import es.pedropareja.database.generic.querygen.base.QGQuery;
+import es.pedropareja.database.generic.querygen.merge.using.QGLinkMergeUsing;
+
+public interface QGMerge
+    extends QGQuery, QGLinkMergeUsing
+{}
---
+++ src/main/java/es/pedropareja/database/generic/querygen/merge/QGMergePrv.java
@@ -0,0 +1,22 @@
+package es.pedropareja.database.generic.querygen.merge;
+
+import es.pedropareja.database.generic.DBFieldInfo;
+import es.pedropareja.database.generic.querygen.base.QGQueryInit;
+
+public class QGMergePrv<T extends Enum<?> & DBFieldInfo> extends QGQueryInit implements QGMerge
+{
+    private final Class<T> tableType;
+
+    public QGMergePrv(Class<T> tableType)
+    {
+        this.tableType = tableType;
+        setFullNamespaces();
+    }
+
+    @Override
+    public <T> void genOutput(StringBuilder stringBuilder, T context)
+    {
+        stringBuilder.append("MERGE INTO ");
+        printTablePath(stringBuilder, tableType, context);
+    }
+}
