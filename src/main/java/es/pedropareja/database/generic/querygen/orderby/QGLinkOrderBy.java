60df32a661c08f7515cb604d6629a7ffb59f0874
Pedro Francisco Pareja Ruiz <pedropareja@gmail.com>
2019-12-13 11:56:31 +0100

    OrderBy: now uses expressions

--- src/main/java/es/pedropareja/database/generic/querygen/orderby/QGLinkOrderBy.java
+++ src/main/java/es/pedropareja/database/generic/querygen/orderby/QGLinkOrderBy.java
@@ -3,16 +3,17 @@ package es.pedropareja.database.generic.querygen.orderby;
 import es.pedropareja.database.generic.DBFieldInfo;
 import es.pedropareja.database.generic.querygen.Order;
 import es.pedropareja.database.generic.querygen.base.QGLinkBase;
+import es.pedropareja.database.generic.querygen.expression.base.QGExpression;
 
 public interface QGLinkOrderBy extends QGLinkBase
 {
-    default <T extends Enum<?> & DBFieldInfo> QGOrderBy orderBy(T field)
+    default QGOrderBy orderBy(QGExpression exp)
     {
-        return assignNext(new QGOrderByPrv<>(field, getInit()));
+        return assignNext(new QGOrderByPrv(exp, getInit()));
     }
 
-    default <T extends Enum<?> & DBFieldInfo> QGOrderBy orderBy(T field, Order order)
+    default QGOrderBy orderBy(QGExpression exp, Order order)
     {
-        return assignNext(new QGOrderByPrv<>(field, order, getInit()));
+        return assignNext(new QGOrderByPrv(exp, order, getInit()));
     }
 }
