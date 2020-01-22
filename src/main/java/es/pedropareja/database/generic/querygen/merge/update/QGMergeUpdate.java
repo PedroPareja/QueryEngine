bcdf63314182758b7a12dde696d41a09bf96d2eb
Pedro Francisco Pareja Ruiz <pedropareja@gmail.com>
2020-01-22 12:23:51 +0100

    Merge fix: update now links to when

--- src/main/java/es/pedropareja/database/generic/querygen/merge/update/QGMergeUpdate.java
+++ src/main/java/es/pedropareja/database/generic/querygen/merge/update/QGMergeUpdate.java
@@ -3,8 +3,9 @@ package es.pedropareja.database.generic.querygen.merge.update;
 import es.pedropareja.database.generic.DBFieldInfo;
 import es.pedropareja.database.generic.querygen.base.QGQuery;
 import es.pedropareja.database.generic.querygen.expression.base.QGExpression;
+import es.pedropareja.database.generic.querygen.merge.when.QGLinkMergeWhen;
 
-public interface QGMergeUpdate extends QGQuery
+public interface QGMergeUpdate extends QGQuery, QGLinkMergeWhen
 {
     QGMergeUpdate set(DBFieldInfo field, QGExpression value);
 }
