2e2146be29c775ef61588aa1c70fdbae4810e081
Pedro Francisco Pareja Ruiz <pedropareja@gmail.com>
2020-01-28 12:36:09 +0100

    Set: link methods expanded

--- src/main/java/es/pedropareja/database/generic/querygen/set/QGLinkSet.java
+++ src/main/java/es/pedropareja/database/generic/querygen/set/QGLinkSet.java
@@ -2,6 +2,7 @@ package es.pedropareja.database.generic.querygen.set;
 
 import es.pedropareja.database.generic.DBFieldInfo;
 import es.pedropareja.database.generic.querygen.base.QGLinkBase;
+import es.pedropareja.database.generic.querygen.expression.base.QGExpression;
 
 public interface QGLinkSet extends QGLinkBase
 {
@@ -9,5 +10,15 @@ public interface QGLinkSet extends QGLinkBase
     default QGSet set(DBFieldInfo... fieldList)
     {
         return assignNext(new QGSetPrv(getInit(), fieldList));
+    }
+
+    default QGSet set(DBFieldInfo field, QGExpression value)
+    {
+        return assignNext(new QGSetPrv(getInit(), field, value));
+    }
+
+    default QGSet set(QGSetAssignment ... setAssignments)
+    {
+        return assignNext(new QGSetPrv(getInit(), setAssignments));
     }
 }
--- src/main/java/es/pedropareja/database/generic/querygen/set/QGSet.java
+++ src/main/java/es/pedropareja/database/generic/querygen/set/QGSet.java
@@ -8,7 +8,7 @@ import es.pedropareja.database.generic.querygen.where.QGLinkWhere;
 public interface QGSet
         extends QGQuery, QGLinkWhere
 {
-    QGSet set(DBFieldInfo field);
+    QGSet set(DBFieldInfo ... fields);
     QGSet set(DBFieldInfo field, QGExpression value);
     QGSet set(QGSetAssignment ... setAssignments);
 }
