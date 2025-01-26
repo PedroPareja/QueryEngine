9891f52e327ca029d4c52e33cae79ac746775081
PedroPareja <PedroPareja@gmail.com>
2025-01-26 16:28:19 +0100

    Null values can be set

--- src/main/java/com/github/pedropareja/database/generic/QueryParamsHelper.java
+++ src/main/java/com/github/pedropareja/database/generic/QueryParamsHelper.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.Collection;
 
 public class QueryParamsHelper
@@ -16,6 +17,15 @@ public class QueryParamsHelper
     protected static final ParamSetter<Boolean> booleanSetter = (s,i,v) -> s.setBoolean(i, v);
     protected static final ParamSetter<Timestamp> timestampSetter = (s,i,v) -> s.setTimestamp(i, v);
 
+    protected static final int TYPE_LONG = Types.NUMERIC;
+    protected static final int TYPE_INT = Types.NUMERIC;
+    protected static final int TYPE_SHORT = Types.NUMERIC;
+    protected static final int TYPE_STRING = Types.VARCHAR;
+    protected static final int TYPE_BIGDECIMAL = Types.NUMERIC;
+    protected static final int TYPE_BOOLEAN = Types.NUMERIC;
+    protected static final int TYPE_TIMESTAMP = Types.TIMESTAMP;
+    
+
     protected final PreparedStatement statement;
     protected int paramIndex = 1;
 
@@ -24,29 +34,31 @@ public class QueryParamsHelper
         this.statement = statement;
     }
 
-    protected <T> void setParam(ParamSetter<T> paramSetter, T value) throws SQLException
+    protected <T> void setParam(ParamSetter<T> paramSetter, int sqlType, T value) throws SQLException
     {
         if(value != null)
             paramSetter.setParam(statement, paramIndex++, value);
+        else
+            setNull(sqlType);
     }
 
-    protected <T> void setParam(ParamSetter<T> paramSetter, T value, boolean condition) throws SQLException
+    protected <T> void setParam(ParamSetter<T> paramSetter, int sqlType, T value, boolean condition) throws SQLException
     {
         if(condition)
-            setParam(paramSetter, value);
+            setParam(paramSetter, sqlType, value);
     }
 
-    protected <T> void setAllParam(ParamSetter<T> paramSetter, Collection<T> collection) throws SQLException
+    protected <T> void setAllParam(ParamSetter<T> paramSetter, int sqlType, Collection<T> collection) throws SQLException
     {
         if(collection != null)
             for(T element: collection)
-                setParam(paramSetter, element);
+                setParam(paramSetter, sqlType, element);
     }
 
-    protected <T> void setAllParam(ParamSetter<T> paramSetter, Collection<T> collection, boolean condition) throws SQLException
+    protected <T> void setAllParam(ParamSetter<T> paramSetter, int sqlType, Collection<T> collection, boolean condition) throws SQLException
     {
         if(condition)
-            setAllParam(paramSetter, collection);
+            setAllParam(paramSetter, sqlType, collection);
     }
 
     public void setNull(int sqlType) throws SQLException
@@ -74,142 +86,142 @@ public class QueryParamsHelper
 
     public void setLong(Long value) throws SQLException
     {
-        setParam(longSetter, value);
+        setParam(longSetter, TYPE_LONG, value);
     }
 
     public void setLong(Long value, boolean condition) throws SQLException
     {
-        setParam(longSetter, value, condition);
+        setParam(longSetter, TYPE_LONG, value, condition);
     }
 
     public void setAllLong(Collection<Long> collection) throws SQLException
     {
-        setAllParam(longSetter, collection);
+        setAllParam(longSetter, TYPE_LONG, collection);
     }
 
     public void setAllLong(Collection<Long> collection, boolean condition) throws SQLException
     {
-        setAllParam(longSetter, collection, condition);
+        setAllParam(longSetter, TYPE_LONG, collection, condition);
     }
 
     public void setInt(Integer value) throws SQLException
     {
-        setParam(intSetter, value);
+        setParam(intSetter, TYPE_INT, value);
     }
 
     public void setInt(Integer value, boolean condition) throws SQLException
     {
-        setParam(intSetter, value, condition);
+        setParam(intSetter, TYPE_INT, value, condition);
     }
 
     public void setAllInt(Collection<Integer> collection) throws SQLException
     {
-        setAllParam(intSetter, collection);
+        setAllParam(intSetter, TYPE_INT, collection);
     }
 
     public void setAllInt(Collection<Integer> collection, boolean condition) throws SQLException
     {
-        setAllParam(intSetter, collection, condition);
+        setAllParam(intSetter, TYPE_INT, collection, condition);
     }
 
     public void setShort(Short value) throws SQLException
     {
-        setParam(shortSetter, value);
+        setParam(shortSetter, TYPE_SHORT, value);
     }
 
     public void setShort(Short value, boolean condition) throws SQLException
     {
-        setParam(shortSetter, value, condition);
+        setParam(shortSetter, TYPE_SHORT, value, condition);
     }
 
     public void setAllShort(Collection<Short> collection) throws SQLException
     {
-        setAllParam(shortSetter, collection);
+        setAllParam(shortSetter, TYPE_SHORT, collection);
     }
 
     public void setAllShort(Collection<Short> collection, boolean condition) throws SQLException
     {
-        setAllParam(shortSetter, collection, condition);
+        setAllParam(shortSetter, TYPE_SHORT, collection, condition);
     }
 
     public void setString(String value) throws SQLException
     {
-        setParam(stringSetter, value);
+        setParam(stringSetter, TYPE_STRING, value);
     }
 
     public void setString(String value, boolean condition) throws SQLException
     {
-        setParam(stringSetter, value, condition);
+        setParam(stringSetter, TYPE_STRING, value, condition);
     }
 
     public void setAllString(Collection<String> collection) throws SQLException
     {
-        setAllParam(stringSetter, collection);
+        setAllParam(stringSetter, TYPE_STRING, collection);
     }
 
     public void setAllString(Collection<String> collection, boolean condition) throws SQLException
     {
-        setAllParam(stringSetter, collection, condition);
+        setAllParam(stringSetter, TYPE_STRING, collection, condition);
     }
 
     public void setBigDecimal(BigDecimal value) throws SQLException
     {
-        setParam(bigDecimalSetter, value);
+        setParam(bigDecimalSetter, TYPE_BIGDECIMAL, value);
     }
 
     public void setBigDecimal(BigDecimal value, boolean condition) throws SQLException
     {
-        setParam(bigDecimalSetter, value, condition);
+        setParam(bigDecimalSetter, TYPE_BIGDECIMAL, value, condition);
     }
 
     public void setAllBigDecimal(Collection<BigDecimal> collection) throws SQLException
     {
-        setAllParam(bigDecimalSetter, collection);
+        setAllParam(bigDecimalSetter, TYPE_BIGDECIMAL, collection);
     }
 
     public void setAllBigDecimal(Collection<BigDecimal> collection, boolean condition) throws SQLException
     {
-        setAllParam(bigDecimalSetter, collection, condition);
+        setAllParam(bigDecimalSetter, TYPE_BIGDECIMAL, collection, condition);
     }
 
     public void setBoolean(Boolean value) throws SQLException
     {
-        setParam(booleanSetter, value);
+        setParam(booleanSetter, TYPE_BOOLEAN, value);
     }
 
     public void setBoolean(Boolean value, boolean condition) throws SQLException
     {
-        setParam(booleanSetter, value, condition);
+        setParam(booleanSetter, TYPE_BOOLEAN, value, condition);
     }
 
     public void setAllBoolean(Collection<Boolean> collection) throws SQLException
     {
-        setAllParam(booleanSetter, collection);
+        setAllParam(booleanSetter, TYPE_BOOLEAN, collection);
     }
 
     public void setAllBoolean(Collection<Boolean> collection, boolean condition) throws SQLException
     {
-        setAllParam(booleanSetter, collection, condition);
+        setAllParam(booleanSetter, TYPE_BOOLEAN, collection, condition);
     }
 
     public void setTimestamp(Timestamp value) throws SQLException
     {
-        setParam(timestampSetter, value);
+        setParam(timestampSetter, TYPE_TIMESTAMP, value);
     }
 
     public void setTimestamp(Timestamp value, boolean condition) throws SQLException
     {
-        setParam(timestampSetter, value, condition);
+        setParam(timestampSetter, TYPE_TIMESTAMP, value, condition);
     }
 
     public void setAllTimestamp(Collection<Timestamp> collection) throws SQLException
     {
-        setAllParam(timestampSetter, collection);
+        setAllParam(timestampSetter, TYPE_TIMESTAMP, collection);
     }
 
     public void setAllTimestamp(Collection<Timestamp> collection, boolean condition) throws SQLException
     {
-        setAllParam(timestampSetter, collection, condition);
+        setAllParam(timestampSetter, TYPE_TIMESTAMP, collection, condition);
     }
 
     public <U, F extends Enum<?> & DBFilterProcessor<U>> void applyFilter(Class<F> filterProcessorType, U filter)
